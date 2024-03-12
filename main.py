6aa8e688becfb5074926a81e5010fea7395a7b0a
Tima-Ameba <zangievtimoha@gmail.com>
2024-03-12 11:43:17 +0600

    обновление

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivy.lang.builder import Builder
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
 
+from kivymd.app import MDApp
+from kivymd.uix.button import MDButton, MDButtonText
+from kivymd.uix.dialog import (
+    MDDialog,
+    MDDialogIcon,
+    MDDialogHeadlineText,
+    MDDialogSupportingText,
+    MDDialogButtonContainer,
+    MDDialogContentContainer,
+)
+from kivymd.uix.divider import MDDivider
+from kivymd.uix.list import (
+    MDListItem,
+    MDListItemLeadingIcon,
+    MDListItemSupportingText,
+)
 Builder.load_file("main.kv")
 
 class MainScreen(MDScreen):
-    pass
+    def show_dialog
+    MDDialog(
+            title="Discard draft?",
+            text="This will reset your device to its default factory settings.",
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL",
+                    theme_text_color="Custom",
+                    text_color=self.theme_cls.primary_color,
+                ),
+                MDFlatButton(
+                    text="DISCARD",
+                    theme_text_color="Custom",
+                    text_color=self.theme_cls.primary_color,
+                ),
+            ],
+        ).open()
 
 class MyToDoList(MDApp):
     def build(self):
