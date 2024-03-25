c9d737c273c55819f877000c06d54f972ca67916
Tima-Ameba <zangievtimoha@gmail.com>
2024-03-25 13:34:43 +0600

    finish!

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ from kivymd.uix.list import (
 from kivymd.uix.button import MDButton, MDButtonText
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.widget import Widget
-Builder.load_file("main.kv")
+
 import json
+
+
+Builder.load_file("main.kv")
+
 
 class MyContent(MDDialogContentContainer):
     pass
@@ -59,31 +63,66 @@ class MainScreen(MDScreen):
         )
         self.dialog.open()
     def done_check(self,l):
-        self.ids.list.remove_widget(l)
-        self.ids.donelist.add_widget(l)
+        
+        if not l.children[0].children[0].active:
+            self.ids.donelist.remove_widget(l)
+            self.ids.list.add_widget(l)
+        else:
+            self.ids.list.remove_widget(l)
+            self.ids.donelist.add_widget(l)
         
     def add(self):
         text1=self.content.ids.todotext.text
-        text2=self.content.ids.todotext_two.text
-        check=MDListItemTrailingCheckbox(
-            )
+        check= MDListItemTrailingCheckbox()
         l=MDListItem(
             MDListItemHeadlineText(
                 text=text1,
             ),
-            MDListItemSupportingText(
-                text=text2,
-            ),
-            check,
+           check,
         )
         check.on_release=lambda x=l:self.done_check(x)
         l.radius=l.height//2
         self.ids.list.add_widget(l)
-        self.dialog.dismiss()\
-            
+        self.dialog.dismiss()
+
 class MyToDoList(MDApp):
+    def on_stop(self):
+        file={}
+        for i in self.mainwindow.ids.list.children:
+            text=i.children[1].children[0].children[0].text
+            check=i.children[0].children[0].active
+            file[text]=int(check)
+        for i in self.mainwindow.ids.donelist.children:
+            text=i.children[1].children[0].children[0].text
+            check=i.children[0].children[0].active
+            file[text]=int(check)
+        with open("todo.json","w") as f:
+            f.write(json.dumps(file))
     def build(self):
-        return MainScreen()
-    
+        self.mainwindow=MainScreen()
+        try:
+            with open("todo.json", "+a",encoding='utf-8') as f:
+                todolist=json.load(f)
+                for i in todolist:
+                    text1=i
+                    check= MDListItemTrailingCheckbox(active=bool(todolist[i]))
+                    l=MDListItem(
+                        MDListItemHeadlineText(
+                            text=text1,
+                        ),
+                    check,
+                    )
+                    check.on_release=lambda x=l:self.mainwindow.done_check(x)
+                    l.radius=l.height//2
+                    if check.active:
+                        self.mainwindow.ids.donelist.add_widget(l)
+                    else:
+                        self.mainwindow.ids.list.add_widget(l)
+            
+        
+        except:
+            pass
+        return self.mainwindow
+       
 if __name__== "__main__":
     MyToDoList().run()
